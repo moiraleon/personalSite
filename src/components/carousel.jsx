bdf4bdc758049ef7b27c9cbb91a64fe3b1a08928
Moira Leon <moira.leon2@gmail.com>
2022-03-02 23:21:27 -0700

    nav

---
+++ src/components/carousel.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class carousel extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <React.Fragment>
+                <div className='carousel slide'></div>
+
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default ;
